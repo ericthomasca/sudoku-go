5e69a759feb1341a8c38b920c5b09a389681e0a7
Eric Thomas <eric@ericthomas.ca>
2024-02-22 20:03:34 -0330

    add cors headers

--- main.go
+++ main.go
@@ -53,6 +53,10 @@ func solveHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 
 		w.WriteHeader(http.StatusOK)
 		w.Write(responseJSON)
